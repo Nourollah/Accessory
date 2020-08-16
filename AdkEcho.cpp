655c38a2ed40ca7acd1056460d91abc3d97db052
AmirMasoud Nourollah <nourollah.am@gmail.com>
2020-08-16 11:10:06 +0430

    Init Commit

---
+++ AdkEcho.cpp
@@ -0,0 +1,70 @@
+/**
+Android USB connection sample.
+
+@Author kotemru.org
+@Licence apache/2.0
+*/
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+
+#include "AOA/AOA.h"
+
+// USB Connector opend.
+AOA acc("Google, Inc.",   // manufacturer
+        "DemoKit",        // model
+        "UsbAccessorySample for ADK", // description
+        "2.0",            // version
+        "https://github.com/mapyo/UsbAccessorySample", // url
+        "000000000000001") ;
+
+/**
+ * Disconnect USB INNTERRUPT aborted.
+ */
+void signal_callback_handler(int signum)
+{
+    fprintf(stderr, "\ninterrupt %d\n",signum);
+    acc.disconnect();
+    exit(0);
+}
+
+static void error(char *msg, int rc) {
+	fprintf(stderr,"Error(%d,%s): %s\n",rc,strerror(errno),msg);
+	acc.disconnect();
+	exit(0);
+}
+
+
+/**
+ * Main function.
+ */
+int main(int argc, char *argv[])
+{
+    signal(SIGINT, signal_callback_handler);
+    signal(SIGTERM, signal_callback_handler);
+
+	unsigned char buff[1024];
+
+	acc.connect(100);
+    printf("\ngo next step\n");
+    FILE *audio;
+    audio = fopen("/home/amirmasoud/Downloads/audio","r");
+
+	// Echo back.
+	while (1) {
+		int len = acc.read(buff, sizeof(buff), 1000000);
+		if (len < 0) error((char*)"acc.read",len);
+		buff[len+1] = '\0';
+		printf("USB>%s\n", buff);
+		// for (int i=0; i<len; i++) buff[i] = buff[i] - 0x20;
+		acc.write(buff, len, 1000);
+	}
+
+
+
+}
